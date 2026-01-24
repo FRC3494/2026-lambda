501696115444ea6ca0ca7149d0de33f1c6ed93a7
liujip0 <liujip2020@gmail.com>
2026-01-23 19:18:54 -0500

    add BuildConstants.java to .gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-lambda";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "8a37e68068be379fcf6dc3a6bb081636ad1f8f56";
-  public static final String GIT_DATE = "2026-01-22 20:12:38 EST";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "40000fb748acf5f1316001e07f9c0d688e2c400b";
+  public static final String GIT_DATE = "2026-01-22 20:29:30 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-22 20:27:17 EST";
-  public static final long BUILD_UNIX_TIME = 1769131637837L;
+  public static final String BUILD_DATE = "2026-01-23 19:18:33 EST";
+  public static final long BUILD_UNIX_TIME = 1769213913004L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
